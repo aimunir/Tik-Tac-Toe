724a5026a3d9e76eec0c88887abad330633f9331
aimunir <aimunir25@outlook.com>
2022-04-27 16:23:41 +0600

    added logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,31 @@
 console.log("welcome to tik tac toe");
-let music=new Audio("tap_sound.wav");
+let audioTurn=new Audio("tap_sound.mp3");
 
 let turn="X";
 
-changeTurn()
+//   function to change turn
+const changeTurn= () =>{
+   return turn === "X"?"0":"X" ;
+}
+
+// Function to check the win
+
+ const checkWin= () =>{
+
+ }
+
+//  Logic
+
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach( element => {
+            let boxtext =element.querySelector('.boxtext');
+            element.addEventListener('click',()=>{
+                if (boxtext.innerText === '') {
+                    boxtext.innerText=turn;
+                     turn=changeTurn();
+                     audioTurn.play();
+                     checkWin();
+                     document.getElementsByClassName("info")[0].innerText= "Turn for " +turn;
+                }
+            })
+})
