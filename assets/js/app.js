538738a5b8b09f59580dfe873fcbab6e5c8d9427
aimunir <aimunir25@outlook.com>
2022-04-27 23:51:50 +0600

    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,8 +30,9 @@ const changeTurn= () =>{
 
 wins.forEach(e=>{
 if ((boxtext[e[0]].innerText===boxtext[e[1]].innerText) &&(boxtext[e[2]].innerText===boxtext[e[1]].innerText) && (boxtext[e[0]].innerText!== "")) {
-    document.querySelector('.info').innerText=boxtext[e[0]].innerText +" won"
+    document.querySelector('.info').innerText=boxtext[e[0]].innerText +" win"
     gameOver=true;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="110px";
 }
 
 })
